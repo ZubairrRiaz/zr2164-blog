1d33c88b2eb7364ba559227709cca558fa771b11
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2024-12-25 20:43:49 +0500

    Add files via upload

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 
 const md = MarkdownIt()
 
-async function fetchPosts(slug: string): Promise<PostData | undefined> {
+async function fetchPosts(slug: string){
     const posts = getAllPosts()
-    return posts.find((post) => post.slug === slug) as PostData | undefined
+    return posts.find((post) => post.slug === slug)
 }
 
 interface PostParams {
@@ -26,14 +26,14 @@ interface PostData {
 
 export default async function Post({ params }: PostParams) {
 
-    const post: PostData | undefined = await fetchPosts(params.slug)
+    const post = await fetchPosts(params.slug)
 
     if (!post) notFound()
 
     const htmlConverter = md.render(post.content)
 
     return (
-        <article className="sm:max-w-4xl mx-auto sm:px-16 px-4">
+        <article className="sm:max-w-4xl mx-auto sm:px-16 px-4 prose">
             <h1 className="font-bold text-3xl text-center font-serif py-5 bg-black text-white rounded-lg">{post.title}</h1>
             <div className="flex justify-center p-5"><Image src={'/sample-04.png'} alt="pic" height={100} width={500}/></div>
             <div className="prose font-[family-name:var(--font-geist-sans)] text-gray-900 text-lg text-" dangerouslySetInnerHTML={{ __html: htmlConverter }} />
@@ -42,4 +42,52 @@ export default async function Post({ params }: PostParams) {
         </article>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// async function fetchPosts(slug: string): Promise<PostData | undefined> {
+//     const posts = getAllPosts()
+//     return posts.find((post) => post.slug === slug) as PostData | undefined
+// }
+
+// interface PostParams {
+//     params: {
+//         slug: string;
+//     };
+// }
+
+// interface PostData {
+//     title: string;
+//     content: string;
+//     date: string;
+//     slug: string; // Ensure slug is part of the PostData if needed
+// }
+
+// export default async function Post({ params }: PostParams) {
+
+//     const post: PostData | undefined = await fetchPosts(params.slug)
+
+//     if (!post) notFound()
+
+//     const htmlConverter = md.render(post.content)
+
+//     return (
+//         <article className="sm:max-w-4xl mx-auto sm:px-16 px-4">
+//             <h1 className="font-bold text-3xl text-center font-serif py-5 bg-black text-white rounded-lg">{post.title}</h1>
+//             <div className="flex justify-center p-5"><Image src={'/sample-04.png'} alt="pic" height={100} width={500}/></div>
+//             <div className="prose font-[family-name:var(--font-geist-sans)] text-gray-900 text-lg text-" dangerouslySetInnerHTML={{ __html: htmlConverter }} />
+//             <h1>{post.title}</h1>
+//             <p>{post.date}</p>
+//         </article>
+//     )
+// }
 
